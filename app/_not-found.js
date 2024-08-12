53998f06683e1d8a69eba9f428302a956961ddcc
devops <devops@qsl>
2024-08-12 13:21:06 +0630

    first commit

---
+++ app/_not-found.js
@@ -0,0 +1,15 @@
+// app/_not-found.tsx
+
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link href="/">
+        <a>Go back to Home</a>
+      </Link>
+    </div>
+  );
+}
